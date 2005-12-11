7b770814ea9cf854cfe30d521c940a85009d54a7
a.rottmann <a.rottmann@gmx.at>
2005-12-11 16:19:45 +0100

    Added SPEDL-FILES clause to system definition
    darcs-hash:20051211151945-1cc0d-d71795d24c6bdff3a207ef7e232fb12cb3f30e5b.gz

--- sys-def.scm
+++ sys-def.scm
@@ -1,4 +1,8 @@
+;;@ A portability library. It offers a single interface to
+;; functionality commonly present, but not standardized in various
+;; Scheme implementations.
 (define-system spells (0 1)
-  (config-files interfaces s48-packages))
+  (config-files interfaces s48-packages)
+  (spedl-files interfaces packages))
 
 ;; arch-tag: 8717975c-90a7-4442-87e2-e2ba9efecca3
