081b8be7e0b3c3ee122aca1b523106794192d309
Andreas Rottmann <rotty@debian.org>
2006-05-23 15:01:50 +0200

    Added missing pathname test
    darcs-hash:20060523130150-b4420-4a5306499afafd0af9fe1d38b50d39f55c206b05.gz

---
+++ tests/pathname.scm
@@ -0,0 +1,35 @@
+;; file.scm -- unit tests for file.scm
+;; arch-tag: aaeb0020-2f1a-11d9-a288-00404513c0a4
+
+;; Copyright (C) 2004, 2005-2006 by Free Software Foundation, Inc.
+
+;; Author: Andreas Rottmann <a.rottmann@gmx.at>
+;; Start date: Fri Nov 05, 2004 12:05
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU Lesser General Public License as published by
+;; the Free Software Foundation; either version 2.1 of the License, or
+;; (at your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+;; This presumes unix namestrings
+(testeez
+ "Path name handling"
+
+ (test/equal "make-pathname" (x->namestring
+                              (make-pathname #f '("foo" "bar") "baz")) "foo/bar/baz")
+ (test/equal "make-pathname" (x->namestring
+                              (make-pathname '/ '("foo") "bar")) "/foo/bar"))
+
+
+;;; file.scm ends here
