220ac0570193d1ce572d5eef14ed4fa51d535d9f
Andreas Rottmann <rotty@debian.org>
2008-09-26 19:40:49 +0200

    sys-def.scm updated
    darcs-hash:20080926174049-b4420-f06f0751a18ca194e573743ef459e31f4b610896.gz

--- sys-def.scm
+++ sys-def.scm
@@ -3,6 +3,36 @@
 ;; Scheme implementations.
 (define-system spells
   (config-files interfaces s48-packages)
-  (spedl-files interfaces packages))
+  (spedl-files interfaces packages)
+  (structures spells.parameter
+              spells.define-values
+              spells.opt-args
+              spells.condition
+              spells.cut
+              spells.error
+              spells.operations
+              spells.table
+              spells.alist
+              spells.vector-lib
+              spells.cells
+              spells.weak
+              spells.byte-vectors
+              spells.blobs
+              spells.streams
+              spells.format
+              spells.pretty-print
+              spells.ascii
+              spells.pregexp
+              spells.port
+              spells.block-io
+              spells.filesys
+              spells.sysutils
+              spells.process
+              spells.bitwise
+              spells.assert
+              spells.hash
+              spells.misc
+              spells.match)
+  (internal-structures spells.define-record-type*-expander))
 
 ;; arch-tag: 8717975c-90a7-4442-87e2-e2ba9efecca3
