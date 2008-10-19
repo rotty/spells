25b9fa5606a8be8a321041fc230b85c34c5976fa
Andreas Rottmann <rotty@debian.org>
2008-10-19 13:37:59 +0200

    Added autogen.sh, .boring fixup
    darcs-hash:20081019113759-b4420-91846d5a76e55273e83a9c0ab0cb34d5f684a03e.gz

---
+++ autogen.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="spells"
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/autogen.sh) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+aclocal
+automake --add-missing
+autoconf
+
+./configure "$@"
+
+
+
+
