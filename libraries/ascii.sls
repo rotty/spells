5bca0c09549567bf37e2a3bc0414bb30aa09112b
Andreas Rottmann <rotty@debian.org>
2008-12-31 15:30:18 +0100

    (spells ascii): Resurrected
    darcs-hash:20081231143018-b4420-db565d46a4130c1d511cdfb8434452b86a8cc7ca.gz

---
+++ libraries/ascii.sls
@@ -0,0 +1,11 @@
+#!r6rs
+
+(library (spells ascii)
+  (export char->ascii ascii->char
+          ascii-limit ascii-whitespaces
+          ascii-upper? ascii-lower?
+          ascii-uppercase ascii-lowercase)
+  (import (rnrs base)
+          (spells include))
+
+  (include-file ((spells scheme) ascii)))
--- scheme/ascii.scm
+++ scheme/ascii.scm
@@ -1,7 +1,7 @@
 ;; ascii.scm -- ASCII encoding
 ;; arch-tag: 75433a58-dc9a-469c-b127-8d1b4edc7124
 
-;; Copyright (C) 2005, 2007 by Free Software Foundation, Inc.
+;; Copyright (C) 2005, 2007, 2008 by Free Software Foundation, Inc.
 
 ;; Author: Andreas Rottmann <rotty@debian.org>
 ;; Start date: Fri Jul 31, 2005 16:25
@@ -27,9 +27,35 @@
 
 ;;@ Converts an ASCII code (integer) into the corresponding
 ;;  character.
-(define (ascii->char n) (proc-to-be-defined))
+(define (ascii->char n)
+  (integer->char n))
 
 ;;@ Converts the character @1 into the corresponding ASCII code.
-(define (char->ascii c) (proc-to-be-defined))
+(define (char->ascii c)
+  (char->integer c))
+
+(define ascii-limit 128)
+
+;;@ List of integers that are considered white-space.
+(define ascii-whitespaces '(32 10 9 12 13)) ;space linefeed tab page return
+
+(define ascii-lowercase-a (char->ascii #\a))
+(define ascii-uppercase-a (char->ascii #\A))
+
+;;@ Check whether @1 is in the upper/lower case range of ASCII.
+(define (ascii-upper? n) (<= ascii-lowercase-a n 90))
+(define (ascii-lower? n) (<= ascii-uppercase-a 122))
+
+;;@ Return the ASCII code of the lower/upper-case version of the
+;; character represented by @1 or @1 itself if @1 does not correspond
+;; to an upper/lower-case character.
+(define (ascii-lowercase n)
+  (if (ascii-upper? n)
+      (+ (- n ascii-uppercase-a) ascii-lowercase-a)
+      n))
+(define (ascii-uppercase n)
+  (if (ascii-lower? n)
+      (+ (- n ascii-lowercase-a) ascii-uppercase-a)
+      n))
 
 ;;; ascii.scm ends here
