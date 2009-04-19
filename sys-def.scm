09c82b011a4bde7ce3d659892a88e8f00f6338fc
Andreas Rottmann <rotty@debian.org>
2009-04-19 23:56:55 +0200

    Replace autoconf with conjure
    Ignore-this: 67153fc57821571748667c36b7dc9477
    
    darcs-hash:20090419215655-b4420-c45de01fc528b82cdffa94722973c289a2f41bc9.gz

--- sys-def.scm
+++ sys-def.scm
@@ -2,39 +2,22 @@
 ;; functionality commonly present, but not standardized in various
 ;; Scheme implementations.
 (define-system spells
-  (config-files interfaces s48-packages)
-  (r6rs-libs "libraries")
-  (structures spells.parameter
-              spells.define-values
-              spells.opt-args
-              spells.condition
-              spells.cut
-              spells.error
-              spells.operations
-              spells.table
-              spells.alist
-              spells.vector-lib
-              spells.cells
-              spells.weak
-              spells.byte-vectors
-              spells.blobs
-              spells.streams
-              spells.format
-              spells.pretty-print
-              spells.ascii
-              spells.pregexp
-              spells.port
-              spells.block-io
-              spells.filesys
-              spells.sysutils
-              spells.process
-              spells.bitwise
-              spells.assert
-              spells.hash
-              spells.misc
-              spells.match)
-  (internal-structures spells.define-record-type*-expander)
 
-  (extra-dist "configure"))
+  (r6rs-libs "libraries")
+  (extra-dist "configure")
+
+  (conjure
+   (import (rnrs)
+           (conjure cc)
+           (conjure hostinfo)
+           (conjure dsl))
+
+   (task cc (cc-conf))
+   
+   (task (configure
+          (produce '((("spells" "foreign") "config.sls")
+                     <= (("libraries" "foreign") "config.sls.in")))
+          (fetchers (cc-fetcher 'cc)
+                    (hostinfo-fetcher))))))
 
 ;; arch-tag: 8717975c-90a7-4442-87e2-e2ba9efecca3
